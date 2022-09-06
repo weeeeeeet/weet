7e304eff8744c954a87ca71759223f92ae5294e2
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-09-06 11:52:45 +0900

    UserService 수정

--- src/test/java/com/weet/app/user/service/UserServiceTests.java
+++ src/test/java/com/weet/app/user/service/UserServiceTests.java
@@ -18,8 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.zerock.myapp.domain.BoardDTO;
-import org.zerock.myapp.domain.Criteria;
+
 
 import com.weet.app.exception.DAOException;
 import com.weet.app.exception.ServiceException;
