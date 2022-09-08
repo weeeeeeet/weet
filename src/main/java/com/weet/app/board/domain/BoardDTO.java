d9355d62d16ede63e1c8d7266b1e3c3726790256
shu-rimp <soooojip@gmail.com>
2022-09-08 11:31:13 +0900

    수정: BoardDTO 필드 수정
    -update ts 제거
    -reply count 제거
    -comm id 추가

--- src/main/java/com/weet/app/board/domain/BoardDTO.java
+++ src/main/java/com/weet/app/board/domain/BoardDTO.java
@@ -1,16 +1,13 @@
 package com.weet.app.board.domain;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class BoardDTO {
 	
+	private Integer commId;
 	private String userId;
 	private String commPostTitle;
 	private String commPostContents;
-	private Date commPostUpdateTs;
-	private Integer commReplyCount;
 	private Character commTempsave;
 } // end class
