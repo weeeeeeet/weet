55a33fed0583ea7f881c6d2205627bcc0ec3f241
shu-rimp <soooojip@gmail.com>
2022-09-19 14:00:36 +0900

    추가: 결제정보 저장, 수강정보 저장 쿼리
    - insertPayInfo
    - insertClassListener

--- src/main/java/com/weet/app/pay/mapper/PayMapper.java
+++ src/main/java/com/weet/app/pay/mapper/PayMapper.java
@@ -1,7 +1,13 @@
 package com.weet.app.pay.mapper;
 
+import com.weet.app.exception.DAOException;
+import com.weet.app.pay.domain.PaymentDTO;
+
 public interface PayMapper {
 	
-	
+	// 결제정보 저장
+	public abstract int insertPayInfo(PaymentDTO dto) throws DAOException;
 
+	// 등록정보 저장
+	public abstract int insertClassListener(String classId, String userId) throws DAOException;
 } // end interface
