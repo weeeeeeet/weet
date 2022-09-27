47947f827be21b753a0787698adc23335ed639a1
starrrj <96775458+starrrj@users.noreply.github.com>
2022-09-28 04:48:38 +0900

    수정중: 비밀번호 찾기

--- src/main/java/com/weet/app/user/mapper/UserFindMapper.java
+++ src/main/java/com/weet/app/user/mapper/UserFindMapper.java
@@ -3,6 +3,7 @@ package com.weet.app.user.mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.weet.app.exception.DAOException;
+import com.weet.app.user.domain.TrainerDTO;
 import com.weet.app.user.domain.UserVO;
 
 public interface UserFindMapper {
@@ -11,4 +12,6 @@ public interface UserFindMapper {
 	@Select("SELECT * FROM t_user WHERE user_name= #{userName} AND user_phone= #{userPhone}")
 	public abstract UserVO selectMatchId(String userName, String userPhone) throws DAOException;
 
+	// 비빌번호 변경
+	public abstract Integer updatePwd(TrainerDTO trainerDTO) throws DAOException;
 }
