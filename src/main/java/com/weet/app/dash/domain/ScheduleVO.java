e9fc975cef26faf9054ed505e456781e230abe27
shu-rimp <soooojip@gmail.com>
2022-09-29 05:30:14 +0900

    Update ScheduleVO.java

--- src/main/java/com/weet/app/dash/domain/ScheduleVO.java
+++ src/main/java/com/weet/app/dash/domain/ScheduleVO.java
@@ -10,18 +10,20 @@ public class ScheduleVO {
 	// T_CLASS_SCHEDULE
 	
 	// 1. 클래스 스케쥴 코드
-	private String cs_id;
+	private String csId;
 	// 2. 클래스 id
-	private String class_id;
+	private String classId;
 	// 3. 요일
-	private String cs_day;
+	private String csDay;
 	// 4. 클래스 시작일
-	private Date cs_start_date;
+	private Date csStartDate;
 	// 5. 수업시작 오전 오후 선택 (오전:1, 오후:2)
-	private String cs_start_time_ampm;
+	private String cs_startTimeAmpm;
 	// 6. 수업 시작 시(hh)
-	private Number cs_start_time_hh;
+	private Integer cs_start_time_hh;
 	// 7. 수업 시작 분(mm)
-	private Number cs_start_time_mm;
+	private Integer cs_start_time_mm;
+	
+	private String userId;
 	
 } // end class
