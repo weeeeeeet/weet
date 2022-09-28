734e37b798751c47fee93a265117c2d4282cf2d1
j-hann <j.han77055@gmail.com>
2022-09-29 04:53:47 +0900

    Merge branch 'main' of https://github.com/weeeeeeet/weet.git into jiyeonn

--- src/main/java/com/weet/app/dash/domain/ScheduleVO.java
+++ src/main/java/com/weet/app/dash/domain/ScheduleVO.java
@@ -20,8 +20,11 @@ public class ScheduleVO {
 	// 5. 수업시작 오전 오후 선택 (오전:1, 오후:2)
 	private String cs_start_time_ampm;
 	// 6. 수업 시작 시(hh)
-	private Number cs_start_time_hh;
+	private Integer cs_start_time_hh;
 	// 7. 수업 시작 분(mm)
-	private Number cs_start_time_mm;
+	private Integer cs_start_time_mm;
+	// 8. 유저 id
+	private String user_id;
+	
 	
 } // end class
